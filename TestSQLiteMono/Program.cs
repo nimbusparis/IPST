2bcd7ecdb42cfc26c27b61b295803e1b231de58a
Mandrake <mandrillon@gmail.com>
2015-01-06 22:41:13 +0100

    Add test project for SQLite on Mono Linux

---
+++ TestSQLiteMono/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class Test
+{
+    public static void Main(string[] args)
+    {
+        string connectionString = "URI=file:SqliteTest.db";
+        IDbConnection dbcon;
+        dbcon = (IDbConnection)new SqliteConnection(connectionString);
+        dbcon.Open();
+        IDbCommand dbcmd = dbcon.CreateCommand();
+        // requires a table to be created named employee
+        // with columns firstname and lastname
+        // such as,
+        //        CREATE TABLE employee (
+        //           firstname varchar(32),
+        //           lastname varchar(32));
+        string sql =
+           "SELECT firstname, lastname " +
+           "FROM employee";
+        dbcmd.CommandText = sql;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            string FirstName = reader.GetString(0);
+            string LastName = reader.GetString(1);
+            Console.WriteLine("Name: " +
+                FirstName + " " + LastName);
+        }
+        // clean up
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbcon.Close();
+        dbcon = null;
+    }
+}
